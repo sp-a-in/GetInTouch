de39c6c781ba47bf094b2b0d8adf02370062526a
sp-a-in <sparshjain325@gmail.com>
2022-08-09 04:22:48 +0530

    Day 8 update - Create Meetup Popup

--- src/components/organizeMeet.vue
+++ src/components/organizeMeet.vue
@@ -1,10 +1,61 @@
 <template>
-  <h1>You are in Organize Meet</h1>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1 class="display-2 grey--text lighten-5" >Create a new Meetup</h1>
+          <form @submit.prevent="onSubmit">
+                <v-text-field
+                  v-model="title"
+                  label="Title"
+                  required
+                ></v-text-field>
+                <v-text-field
+                  v-model="location"
+                  label="Location"
+                  required
+                ></v-text-field>
+                <v-text-field
+                  v-model="imageUrl"
+                  label="Image URL"
+                  required
+                ></v-text-field>
+                <img :src="imageUrl" height="150px">
+                <v-textarea
+                  v-model="description"
+                  label="Description"
+                  multi-line
+                  required
+                ></v-textarea>
+                <v-btn class="primary" type="submit">Create</v-btn>
+          </form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return {
+      title: '',
+      location: '',
+      imageUrl: '',
+      description: '',
+    }
+  },
+  methods:{
+    onSubmit(){
+      let payLoad = {
+        id: 'dkjfghsdf23',
+        title: this.title,
+        location: this.location,
+        url: this.imageUrl,
+        description: this.description,
+        date: new Date(),
+      };
+      this.$store.dispatch('createMeetup',payLoad);
+    }
+  }
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -12,13 +12,17 @@ export const store = new Vuex.Store(
                   id : 'jhsfgjhsg4',
                   title : 'Spiderman',
                   url : 'https://c4.wallpaperflare.com/wallpaper/907/35/593/spiderman-hd-4k-5k-wallpaper-preview.jpg',
-                  date : '21st March 2022'
+                  location: 'Banglore',
+                  date : '21st March 2022',
+                  description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore eius eveniet enim? Nesciunt qui laborum rerum? Dicta nemo beatae reprehenderit tempore, eius autem ipsa, magnam porro id asperiores in accusamus obcaecati excepturi, minus iste pariatur facere voluptatum fugiat voluptates eaque quo. Esse repellat, distinctio atque repellendus deserunt autem vitae laborum officiis nulla cum, perspiciatis quod voluptas fugiat et suscipit sapiente facere a aliquam temporibus iure. Modi, assumenda mollitia. Recusandae iure sed saepe nesciunt dignissimos error labore, quos doloremque consectetur at quae voluptatem. Mollitia in voluptates aliquam, est error velit at consequatur assumenda ea sit quos pariatur molestiae possimus eos! Molestiae soluta provident quasi, quia harum nemo error dolorum modi cumque pariatur quos obcaecati accusamus maiores dignissimos numquam quod perspiciatis voluptate minus voluptates asperiores? Porro reiciendis maxime dolorem sint perspiciatis, quod temporibus ab perferendis, recusandae facilis corrupti aut dolore doloremque delectus dolorum quas cum doloribus repudiandae tenetur officia! Voluptas exercitationem ipsa nisi culpa inventore asperiores, deleniti assumenda labore? Velit blanditiis in quibusdam sunt mollitia? Saepe sapiente ullam fuga natus labore mollitia quae cupiditate eligendi repellat consectetur! Maxime cumque eligendi excepturi. Quo ea rem quam dolor, incidunt veniam aspernatur dignissimos cupiditate. At earum, facilis reprehenderit culpa similique accusamus quisquam cupiditate repudiandae expedita.'
                 },
                 {
                   id : 'bfbdkfnj5',
                   title : 'Batman',
                   url : 'https://www.wallpapersshare.com/img/big/batman-superhero-ultra-hd-4k-wallpaper.jpg',
-                  date : '2nd July 2023' 
+                  location: 'Sheopur',
+                  date : '2nd July 2023',
+                  description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore eius eveniet enim? Nesciunt qui laborum rerum? Dicta nemo beatae reprehenderit tempore, eius autem ipsa, magnam porro id asperiores in accusamus obcaecati excepturi, minus iste pariatur facere voluptatum fugiat voluptates eaque quo. Esse repellat, distinctio atque repellendus deserunt autem vitae laborum officiis nulla cum, perspiciatis quod voluptas fugiat et suscipit sapiente facere a aliquam temporibus iure. Modi, assumenda mollitia. Recusandae iure sed saepe nesciunt dignissimos error labore, quos doloremque consectetur at quae voluptatem. Mollitia in voluptates aliquam, est error velit at consequatur assumenda ea sit quos pariatur molestiae possimus eos! Molestiae soluta provident quasi, quia harum nemo error dolorum modi cumque pariatur quos obcaecati accusamus maiores dignissimos numquam quod perspiciatis voluptate minus voluptates asperiores? Porro reiciendis maxime dolorem sint perspiciatis, quod temporibus ab perferendis, recusandae facilis corrupti aut dolore doloremque delectus dolorum quas cum doloribus repudiandae tenetur officia! Voluptas exercitationem ipsa nisi culpa inventore asperiores, deleniti assumenda labore? Velit blanditiis in quibusdam sunt mollitia? Saepe sapiente ullam fuga natus labore mollitia quae cupiditate eligendi repellat consectetur! Maxime cumque eligendi excepturi. Quo ea rem quam dolor, incidunt veniam aspernatur dignissimos cupiditate. At earum, facilis reprehenderit culpa similique accusamus quisquam cupiditate repudiandae expedita.'
                 },
               ],
               user : [
@@ -28,8 +32,18 @@ export const store = new Vuex.Store(
                 }
               ]
         },
-        actions: {},
-        mutations: {},
+        mutations: {
+            createMeetup(state, payload){
+                console.log("Worked");
+                state.allMeetups.push(payload);
+            }
+        },
+        actions: {
+            createMeetup({commit}, payload){
+                console.log("Actionsa");
+                commit('createMeetup', payload);
+            }
+        },
         getters: {
             allMeetups(state){
                 return state.allMeetups;
